81073e14fd55e4c23561514a9e2c76f248d27522
nhanan <65924374+nhanan@users.noreply.github.com>
2024-03-12 08:51:24 +0000

    Create auth.py
    Working OAuth2.0 process for Bullhorn ATS written in Python.
    
    It doesn't use the refresh token process, so needs to be called each time else BHRestToken expires after 10 minutes.

---
+++ auth.py
@@ -0,0 +1,39 @@
+import requests
+from urllib.parse import urlparse
+
+# Consult BH Support to set up API user and get client id + secret
+# Visit https://rest.bullhornstaffing.com/rest-services/loginInfo?username={api_user} to get URLs
+
+api_user = API_USER
+api_pass = API_PASS
+client_id = CLIENT_ID
+client_secret = CLIENT_SECRET
+auth_url = "https://auth.bullhornstaffing.com/" # May need to change these depending on where you're based - see notes above
+rest_url = "https://rest.bullhornstaffing.com/" # May need to change these depending on where you're based - see notes above
+
+def start():
+    return(login(str(getAccessToken())))
+
+def getAccessToken():
+    authCode = getAuthCode()
+    url = f"{auth_url}oauth/token?grant_type=authorization_code&code={authCode}&client_id={client_id}&client_secret={client_secret}"
+    response = requests.request("POST", url)
+    accessToken = response.json()["access_token"]
+    return(accessToken)
+
+def getAuthCode():
+    url = f"{auth_url}oauth/authorize?client_id={client_id}&response_type=code&action=Login&username={api_user}&password={api_pass}"
+    response = requests.request("GET", url)
+    response = urlparse(response.url)
+    authCode = response[4][5:60:]
+    return(authCode)
+
+def login(accessToken):
+    url = f"{rest_url}rest-services/login?version=*&access_token={accessToken}"
+    response = requests.request("POST", url)
+    print("Bullhorn API Login: " + response.reason)
+    return(response.json()["BhRestToken"])
+
+
+# Call start function to authenticate the session, then you can make API calls to your Bullhorn ATS instance
+start()
