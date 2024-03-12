c73a598984eaee2d18422c6ec4711fe68da90607
nhanan <65924374+nhanan@users.noreply.github.com>
2024-03-12 16:56:12 +0000

    Retrieve Auth URL programmatically opposed to manually

--- auth.py
+++ auth.py
@@ -8,21 +8,28 @@ api_user = API_USER
 api_pass = API_PASS
 client_id = CLIENT_ID
 client_secret = CLIENT_SECRET
-auth_url = "https://auth.bullhornstaffing.com/" # May need to change these depending on where you're based - see notes above
 rest_url = "https://rest.bullhornstaffing.com/" # May need to change these depending on where you're based - see notes above
 
 def start():
     return(login(str(getAccessToken())))
 
+def get_cluster_information():
+    rest_url = f"https://rest.bullhornstaffing.com/rest-services/loginInfo?username={api_user}"
+    response = requests.request("POST", rest_url)
+    auth_url = response.json()['oauthUrl']
+    return(auth_url)
+
+auth_url = get_cluster_information()
+
 def getAccessToken():
     authCode = getAuthCode()
-    url = f"{auth_url}oauth/token?grant_type=authorization_code&code={authCode}&client_id={client_id}&client_secret={client_secret}"
+    url = f"{auth_url}/token?grant_type=authorization_code&code={authCode}&client_id={client_id}&client_secret={client_secret}"
     response = requests.request("POST", url)
     accessToken = response.json()["access_token"]
     return(accessToken)
 
 def getAuthCode():
-    url = f"{auth_url}oauth/authorize?client_id={client_id}&response_type=code&action=Login&username={api_user}&password={api_pass}"
+    url = f"{auth_url}/authorize?client_id={client_id}&response_type=code&action=Login&username={api_user}&password={api_pass}"
     response = requests.request("GET", url)
     response = urlparse(response.url)
     authCode = response[4][5:60:]
